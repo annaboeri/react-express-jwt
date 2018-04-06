c833b00eccc95348cd9317f39e61535ca8c1d29e
Anna Boeri <annaboeri@gmail.com>
2018-04-06 11:00:58 -0700

    add bars router

---
+++ routes/bars.js
@@ -0,0 +1,28 @@
+const 
+    express = require('express'),
+    barsRouter = new express.Router(),
+    Bar = require('../models/Bar.js'),
+    { verifyToken } = require('../serverAuth.js') 
+
+
+barsRouter.get('/', (req, res) => {
+    Bar.find({}, (err, bars) => {
+        res.json(bars)
+    })
+})
+
+// ensures that by the time you get to that action, req.user is available
+barsRouter.use(verifyToken)
+
+barsRouter.post('/', (req, res)=> {
+    console.log(req.user)
+    // .create builds the bar object and saves to db in one command
+    // includes all fields from form plus a user key, which is the current user
+    Bar.create({...req.body, user: req.user}, (err, bar) => {
+        res.json({ success: true, message: "bar created", bar: bar})
+
+    })
+})
+
+module.exports = barsRouter
+
